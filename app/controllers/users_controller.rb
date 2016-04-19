ee2f6b12fe8b3fcf0ab7ea18f73f1a7a041186d4
bachtsui <dbtsui2@gmail.com>
2016-04-18 18:30:41 -0700

    Full CRUD for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,46 @@ class UsersController < ApplicationController
     @user = User.friendly.find(params[:id])
   end
 
+  def edit
+    if current_user == set_user
+      render :edit
+    else
+      flash[:error] = @user.errors.full_messages.join(", ")
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "Successfully Updated Profile"
+    else
+      flash[:error] = @user.errors.full_messages.join(", ")
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  def destroy
+    @user = User.friendly.find(params[:id])
+    if current_user == @user
+      @user.destroy
+      flash[:notice] = "Successfully deleted user #{@user.last_name}"
+      redirect_to root_path
+    else
+      flash[:notice] = "You can't delete that user."
+      redirect_to user_path(@user)
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone_number, :email, :username, :password, :password_confirmation)
+  end
+
+  def set_user
+    user_id = params[:id] || current_user.id
+    @user = User.friendly.find(user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/edit", to: "users#edit", as: "edit_user"
+  patch "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
 
   # Routes for Sessions
   get "/login", to: "sessions#new", as: "new_session"   
   get "/logout", to: "sessions#destroy", as: "destroy_session"
   post "/sessions", to: "sessions#create" 
-  # get 'sessions/new'
 end
