d9a8cda460df1489bc5b9351ef7d35c620663612
bachtsui <dbtsui2@gmail.com>
2016-04-18 18:07:49 -0700

    Just finished sessions, need to make edit and delete for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+  
+  before_action :logged_in?, only: [:show, :edit, :update] 
 
 	def index
     @users = User.all
@@ -22,8 +23,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:id])
-    render :show
+    @user = User.friendly.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   #Root
-  # root to: "home#index"
+  root to: "welcome#index"
 
   #Routes for Users
   get "/users", to: "users#index", as: "users"
   get "/signup", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+
+  # Routes for Sessions
+  get "/login", to: "sessions#new", as: "new_session"   
+  get "/logout", to: "sessions#destroy", as: "destroy_session"
+  post "/sessions", to: "sessions#create" 
+  # get 'sessions/new'
 end
