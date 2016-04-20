71dd03c73e45808b43498ad64e20b94ec2d8512a
bachtsui <dbtsui2@gmail.com>
2016-04-20 15:02:58 -0700

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419230215) do
+ActiveRecord::Schema.define(version: 20160420215936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -53,6 +60,13 @@ ActiveRecord::Schema.define(version: 20160419230215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "panswer"
+  end
+
+  create_table "user_contact_joints", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "contact_id"
   end
 
   create_table "users", force: :cascade do |t|
