5ec15cb8c752778898cdce7990818d4f8c541335
bachtsui <dbtsui2@gmail.com>
2016-04-25 14:11:07 -0700

    Added question 6 and 7  to Notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425204708) do
+ActiveRecord::Schema.define(version: 20160425210255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,12 @@ ActiveRecord::Schema.define(version: 20160425204708) do
     t.text     "pathree"
     t.text     "pafour"
     t.text     "pafive"
+    t.string   "qsix"
+    t.string   "qseven"
+    t.text     "asix"
+    t.text     "aseven"
+    t.text     "pasix"
+    t.text     "paseven"
   end
 
   create_table "questions", force: :cascade do |t|
