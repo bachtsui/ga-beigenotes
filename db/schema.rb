adba220274977a4e857dda2a9db5e55ad452f47c
bachtsui <dbtsui2@gmail.com>
2016-04-27 23:47:43 -0700

    I think we're onto something

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425210255) do
+ActiveRecord::Schema.define(version: 20160428063659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20160425210255) do
     t.text     "aseven"
     t.text     "pasix"
     t.text     "paseven"
+    t.integer  "created_by"
   end
 
   create_table "questions", force: :cascade do |t|
